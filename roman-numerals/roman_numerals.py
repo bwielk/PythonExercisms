ec213151c1cd14a655890f0a3c3c84358320e5a5
bwielk <bwielk@gmail.com>
2019-02-26 22:23:35 +0000

    Basic transcription from 1 to 3 works

--- roman-numerals/roman_numerals.py
+++ roman-numerals/roman_numerals.py
@@ -1,2 +1,10 @@
 def numeral(number):
-    pass
+    result = []
+    arabic_to_roman = {1: 'I'}
+    number_to_process = number
+    while number_to_process > 0:
+        for arabic, roman in arabic_to_roman.items():
+            if arabic <= number_to_process:
+                result.append(arabic_to_roman[arabic])
+                number_to_process -= arabic
+    return ''.join(result)
