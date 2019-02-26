21c8b45ff957679c7c18e209c111e8f59e60eb20
bwielk <bwielk@gmail.com>
2019-02-26 22:36:28 +0000

    Translations work up to number 900

--- roman-numerals/roman_numerals.py
+++ roman-numerals/roman_numerals.py
@@ -1,10 +1,22 @@
 def numeral(number):
     result = []
-    arabic_to_roman = {1: 'I'}
+    arabic_to_roman = {
+        500: 'D',
+        400: 'CD',
+        100: 'C',
+        90: 'XC',
+        50: 'L',
+        40: 'XL',
+        10: 'X',
+        9: 'IX',
+        5: 'V',
+        4: 'IV',
+        1: 'I'}
     number_to_process = number
     while number_to_process > 0:
         for arabic, roman in arabic_to_roman.items():
             if arabic <= number_to_process:
-                result.append(arabic_to_roman[arabic])
-                number_to_process -= arabic
+                amount_of_unit = number_to_process // arabic
+                result.append(amount_of_unit*arabic_to_roman[arabic])
+                number_to_process -= amount_of_unit*arabic
     return ''.join(result)
