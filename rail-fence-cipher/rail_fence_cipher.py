c5454d0bae9d9397ea48d13d263d210b4919b930
bwielk <bwielk@gmail.com>
2019-03-13 22:37:37 +0000

    Encoding the rail fences successful for 2 and 3 railed messages

--- rail-fence-cipher/rail_fence_cipher.py
+++ rail-fence-cipher/rail_fence_cipher.py
@@ -1,6 +1,37 @@
 def encode(message, rails):
-    pass
+    result = ''
+    if rails == 2:
+        result += message[::rails]
+        result += message[1::rails]
+    elif rails == 3:
+        result += message[::rails+1]
+        rails -= 1
+        result += message[1::rails]
+        rails += 2
+        result += message[2::rails]
+    elif rails == 4:
+        result += message[::6]
+        second_row_factor = 1
+        index = 1
+        for i in range(0, len(message)):
+            if index <= len(message):
+                result += message[index]
+                second_row_factor = 4 if second_row_factor <= 2 else 2
+                index += second_row_factor
+            else:
+                break
+        second_row_factor = 0
+        index = 2
+        for i in range(0, len(message)):
+            if index <= len(message):
+                result += message[index]
+                second_row_factor = 2 if second_row_factor < 4 else 4
+                index += second_row_factor
+            else:
+                break
 
+
+    return result
 
 def decode(encoded_message, rails):
     pass
---
+++ simple-cipher/simple_cipher.py
@@ -0,0 +1,9 @@
+class Cipher(object):
+    def __init__(self, key=None):
+        pass
+
+    def encode(self, text):
+        return text
+
+    def decode(self, text):
+        pass
